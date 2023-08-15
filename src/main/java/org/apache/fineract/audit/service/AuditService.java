ee0cee3afe5cdd9a381bd5b2366df7bf6c677ee7
Kelvin Thuku <kelvinthuku@Thukus-MacBook-Pro.local>
2023-08-15 10:41:57 +0300

    javadocs added to service  methods

--- src/main/java/org/apache/fineract/audit/service/AuditService.java
+++ src/main/java/org/apache/fineract/audit/service/AuditService.java
@@ -5,10 +5,24 @@ import org.apache.fineract.audit.events.NewAuditEvent;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-
+/**
+ * A service interface for managing audit-related operations.
+ */
 public interface AuditService {
-
+    /**
+     * Creates a new audit entry based on the provided audit event.
+     *
+     * @param event The new audit event containing information for the audit entry.
+     * @return The newly created audit source entry.
+     */
     AuditSource createNewEntry(NewAuditEvent event);
 
+    /**
+     * Retrieves a page of audit entries based on the specified criteria.
+     *
+     * @param specification The specification to filter audit entries.
+     * @param pageable      The pagination information for the result page.
+     * @return A page of audit entries meeting the specified criteria.
+     */
     Page<AuditSource> getAudits(Specification<AuditSource> specification, Pageable pageable);
 }
--- src/main/java/org/apache/fineract/audit/specs/AuditSpec.java
+++ src/main/java/org/apache/fineract/audit/specs/AuditSpec.java
@@ -10,10 +10,18 @@ import java.util.Calendar;
 import java.util.Date;
 
 import static org.springframework.data.jpa.domain.Specification.where;
-
+/**
+ * A component that generates specifications for querying audit entries based on provided search criteria.
+ */
 @Component
 @Slf4j
 public class AuditSpec extends BaseSpecification<AuditSource, AuditSearch> {
+    /**
+     * Generates a specification for filtering audit entries based on the provided search criteria.
+     *
+     * @param request The audit search criteria.
+     * @return A specification for filtering audit entries.
+     */
     @Override
     public Specification<AuditSource> getFilter(AuditSearch request) {
         return (root, query, cb) -> {
@@ -28,6 +36,15 @@ public class AuditSpec extends BaseSpecification<AuditSource, AuditSearch> {
                     .toPredicate(root, query, cb);
         };
     }
+    /**
+     * Gets a date adjusted to a specific time of day.
+     *
+     * @param date The input date.
+     * @param hour The target hour.
+     * @param minute The target minute.
+     * @param seconds The target second.
+     * @return The adjusted date with the specified time of day.
+     */
 
     public static Date getExactDate(Date date, int hour, int minute, int seconds) {
         if (date == null)
@@ -39,7 +56,13 @@ public class AuditSpec extends BaseSpecification<AuditSource, AuditSearch> {
         calendar.set(Calendar.SECOND, seconds);
         return calendar.getTime();
     }
-
+    /**
+     * Generates a specification for filtering audit entries based on the provided attribute and string value.
+     *
+     * @param attribute The attribute to filter on.
+     * @param value The string value to match.
+     * @return A specification for filtering audit entries based on the string attribute and value.
+     */
     private Specification<AuditSource> fieldContains(String attribute, String value) {
         return (root, query, cb) -> {
             if (value == null) {
@@ -51,7 +74,13 @@ public class AuditSpec extends BaseSpecification<AuditSource, AuditSearch> {
             );
         };
     }
-
+    /**
+     * Generates a specification for filtering audit entries based on the provided attribute and long value.
+     *
+     * @param attribute The attribute to filter on.
+     * @param value The long value to match.
+     * @return A specification for filtering audit entries based on the long attribute and value.
+     */
     private Specification<AuditSource> fieldContains(String attribute, Long value) {
         return (root, query, cb) -> {
             if (value == null) {
@@ -61,6 +90,14 @@ public class AuditSpec extends BaseSpecification<AuditSource, AuditSearch> {
         };
     }
 
+    /**
+     * Generates a specification for filtering audit entries based on the provided date range.
+     *
+     * @param field The field to filter on.
+     * @param start The start date of the date range.
+     * @param end The end date of the date range.
+     * @return A specification for filtering audit entries within the specified date range.
+     */
     private Specification<AuditSource> dateBetween(String field, Date start, Date end) {
         return (root, query, cb)
                 -> {
