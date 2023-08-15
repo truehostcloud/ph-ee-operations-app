c79c74d0288689bd4d706ed602ceb9c95b68a3c2
Kelvin Thuku <kelvinthuku@Thukus-MacBook-Pro.local>
2023-08-15 12:53:37 +0300

    field made static

--- src/main/java/org/apache/fineract/audit/specs/BaseSpecification.java
+++ src/main/java/org/apache/fineract/audit/specs/BaseSpecification.java
@@ -3,7 +3,7 @@ package org.apache.fineract.audit.specs;
 import org.springframework.data.jpa.domain.Specification;
 
 public abstract class BaseSpecification<T, U> {
-    private final String wildcard = "%";
+    private static final String wildcard = "%";
 
     public abstract Specification<T> getFilter(U request);
 
