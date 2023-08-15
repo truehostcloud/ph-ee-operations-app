9f5299654ffa28978a66a4358dd4fda6c15cf30b
Kelvin Thuku <kelvinthuku@Thukus-MacBook-Pro.local>
2023-08-15 12:56:54 +0300

    field made static

--- src/main/java/org/apache/fineract/audit/specs/BaseSpecification.java
+++ src/main/java/org/apache/fineract/audit/specs/BaseSpecification.java
@@ -3,11 +3,11 @@ package org.apache.fineract.audit.specs;
 import org.springframework.data.jpa.domain.Specification;
 
 public abstract class BaseSpecification<T, U> {
-    private static final String wildcard = "%";
+    private static final String WILD_CARD = "%";
 
     public abstract Specification<T> getFilter(U request);
 
     protected String containsLowerCase(String searchField) {
-        return wildcard + searchField.toLowerCase() + wildcard;
+        return WILD_CARD + searchField.toLowerCase() + WILD_CARD;
     }
 }
